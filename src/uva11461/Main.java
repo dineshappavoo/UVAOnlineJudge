590448348fe237ca4ed0baec2431dd1c520d6798
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-27 02:07:32 -0500

    Updated uva 11461 implementation

--- src/uva11461/Main.java
+++ src/uva11461/Main.java
@@ -19,23 +19,52 @@ public class Main {
 	public void getInput()
 	{
 		Scanner scanner=new Scanner(System.in);
-		int a, b, count;
-		double value;
-		
-		
+		int a, b, count=0,val1, startSquare=0;
+		double value, newValue, m;
+
+
 		while(scanner.hasNextInt())
 		{
 			a=scanner.nextInt();
 			b=scanner.nextInt();			
 			if(a==0 && b==0)
 				break;
+			
+			value=Math.sqrt(a);
+			val1=(int) Math.sqrt(a);
+			newValue=val1;
+
 			if(a==b)
 			{
-				
-			}
-			while(a<=b)
+				if(value==newValue)
+				{
+					System.out.println("2");
+				}else
+				{
+					System.out.println("0");
+				}
+			}else
 			{
-				value=Math.sqrt(a++);
+				if(value>newValue)
+				{
+					startSquare=val1+1;
+				}else
+				{
+					startSquare=val1;
+				}
+				
+
+				m=Math.pow(startSquare, 2);
+				while(m<=b)
+				{
+					m=Math.pow(startSquare++, 2);
+					if(m<=b)
+					{
+						count++;
+					}
+				}
+				System.out.println(count);
+				count=0;
 			}
 		}
 	}
