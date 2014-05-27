a1f055e38e20529d3622e1cb45ac8b2cfab8ace7
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-26 22:49:57 -0500

    Updated the uva11636 implementation

--- src/uva11636/Main.java
+++ src/uva11636/Main.java
@@ -29,18 +29,33 @@ public class Main {
 			if(n==0||n==1)
 			{
 				System.out.println("Case "+serial+": 0");
+			}else if(n==2)
+			{
+				System.out.println("Case "+serial+": 1");
+
 			}else
 			{
 				int m=(int) Math.pow(2, 1);
 				int i=1;
-				for(i=1;m<n;i++)
+				int value=1,noOfPastes=1;
+				
+				for(i=1;m<=n;i++)
 				{
 					m=(int) Math.pow(2, i);
+					if(m<=n)
+					{
+						value=m;
+						noOfPastes=i;
+					}
 				}
-				if(m==n)
-					System.out.println("Case "+serial+": "+i);
+				if(value==n)
+				{
+					System.out.println("Case "+serial+": "+(noOfPastes));
+				}
 				else
-					System.out.println("Case "+serial+": "+(i+1));
+				{
+					System.out.println("Case "+serial+": "+(noOfPastes+1));
+				}
 			}
 			serial++;
 			
