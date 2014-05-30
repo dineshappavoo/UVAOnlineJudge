fb780612de2b8eb965c0c368d7b806edacfa3cc7
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-29 22:59:52 -0500

    uva 548 impl in java - partial

---
+++ src/uva548/Main.java
@@ -0,0 +1,33 @@
+package uva548;
+/**
+ * 
+ */
+
+
+import java.util.Scanner;
+
+/**
+ * @author Dinesh Appavoo
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		new Main().getInput();
+	}
+
+	public void getInput()
+	{
+		Scanner scanner=new Scanner(System.in);
+		long i, j, diff;
+		
+		while(scanner.hasNextLong())
+		{
+			i=scanner.nextLong();
+			j=scanner.nextLong();
+			
+			
+		}
+	}
+
+}
